29ba31fe496542ce7aec8c9f5ef48ca0fb2a6c8d
Toshizabeth <dearprograms2015@gmail.com>
2021-09-05 18:11:31 +0900

    エラー解決のために一次対応

--- Assets/App/Scripts/Adv/Utility/DestoryCallbacker.cs
+++ Assets/App/Scripts/Adv/Utility/DestoryCallbacker.cs
@@ -4,11 +4,41 @@
 //  
 // Created by toshiki sakamoto on 2020.06.03
 // 
-using Adv.Utility.Extensions;
+using System;
 using UnityEngine;
 
 namespace Adv.Utility
 {
+	/// <summary>
+	/// System.Action, System.Func 等の拡張機能
+	/// </summary>
+	public static class MulticastDelegateExtensions
+	{
+		public static int GetLength(this MulticastDelegate self)
+		{
+			var invocationList = self.GetInvocationList();
+			if (self == null || invocationList == null)
+			{
+				return 0;
+			}
+
+			return invocationList.Length;
+		}
+
+		public static bool IsNullOrEmpty(this MulticastDelegate self) =>
+			self.GetLength() <= 0;
+
+		/// <summary>
+		/// リストに存在するか確認
+		/// </summary>
+		public static bool Exists(this MulticastDelegate self, Delegate target)
+		{
+			if (target == null) return false;
+
+			return System.Array.Exists(self.GetInvocationList(), elm => elm == target);
+		}
+	}
+
 	/// <summary>
 	/// オブジェクトが削除された時を検知する
 	/// </summary>
